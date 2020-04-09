44534b63b2ba47ebea29807a4b88a52a3ca5abc4
Ashok Jethwani <ashok.jethwani@medavie.bluecross.ca>
2020-04-09 13:43:47 -0300

    model - range

--- assign/Models/OrderItem.cs
+++ assign/Models/OrderItem.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace assign.Models
 {
@@ -7,6 +8,7 @@ namespace assign.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+         [Range(1, 149)]
         public int quantity { get; set; }
         public double price { get; set; }
         public double total { get; set; }
